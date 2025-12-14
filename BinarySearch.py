af7b88620eeaa6ce00de8a9554229f5d4e394526
Pianyu666 <2282358328@qq.com>
2025-12-14 14:16:56 +0800

    refactor: safer mid calculation in binary_search

--- BinarySearch.py
+++ BinarySearch.py
@@ -8,7 +8,7 @@ def binary_search(arr, target):
     left, right = 0, len(arr) - 1
 
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
 
         if arr[mid] == target:
             return mid
