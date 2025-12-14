ed5f6a430868a5878a9aa449b5830b5b1d6f96cf
Pianyu666 <2282358328@qq.com>
2025-12-14 14:41:58 +0800

    improve bubble 2

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,23 +1,22 @@
-def bubble_sort(arr):
+def bubble_sort_inplace(arr):
     """
-    冒泡排序（记录最后交换位置优化）
+    原地冒泡排序
     """
     arr = arr.copy()
     n = len(arr)
 
-    while n > 1:
-        last_swap = 0
-        for i in range(1, n):
-            if arr[i - 1] > arr[i]:
-                arr[i - 1], arr[i] = arr[i], arr[i - 1]
-                last_swap = i
-        n = last_swap  # 下一轮只需遍历到最后交换的位置
-
-    return arr
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
 
 
 
 if __name__ == "__main__":
     data = [64, 34, 25, 12, 22, 11, 90]
     print("原始数据:", data)
-    print("排序结果:", bubble_sort(data))
+    print("排序结果:", bubble_sort_inplace(data))
